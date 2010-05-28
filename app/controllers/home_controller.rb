f9b4ed3fab85eee4c8d5bdebc7a1c31e09a95b55
ilake <lake.ilakela+github@gmail.com>
2010-05-28 11:27:09 +0800

    fix no experience view bug

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -14,7 +14,18 @@ class HomeController < ApplicationController
     params[:location] ||= current_user.try(:location_list)
     @user_experiences = current_user.experiences.descend_by_updated_at.limit(5) 
     @latest_experiences = Experience.location_with(params[:location]).descend_by_updated_at.limit(5).all(:include => :user)
-    @near_experience = Experience.tagged_with(current_user.owned_tags.map{|t| t.name}.rand).limit(1)[0]
+    @near_experience = if current_user.owned_tags.blank?
+                         Experience.tally({  :at_least => 1, 
+                                          :at_most => 10000,  
+                                          :start_at => 1.weeks.ago,
+                                          :end_at => 1.day.ago,
+                                          :limit => 1,
+                                          :order => "items.name desc"
+                         }).first
+
+                       else
+                         Experience.tagged_with(current_user.owned_tags.map{|t| t.name}.rand).limit(1).first
+                       end
   end
 
   private
