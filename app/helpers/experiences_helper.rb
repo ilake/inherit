a6fa546f4eaf6ed413c9b7533dd0a9cab48d9cbf
ilake <lake.ilakela+github@gmail.com>
2010-06-14 21:04:36 +0800

    把時間軸開始時間設為生日, 結束設為五年後

--- app/helpers/experiences_helper.rb
+++ app/helpers/experiences_helper.rb
@@ -1,8 +1,9 @@
 module ExperiencesHelper
-  def timeline_start_time(user, experiences)
-    if time = experiences.last.try(:start_at).try(:to_s, :js_date)
-      time
-    elsif time = user.try(:birthday).try(:to_s, :js_date) 
+  def timeline_start_time(user)
+#    if time = experiences.last.try(:start_at).try(:to_s, :js_date)
+#      time
+    #elsif time = user.try(:birthday).try(:to_s, :js_date) 
+    if time = user.try(:birthday).try(:to_s, :js_date) 
       time
     else 
       Time.now.ago(1.years).to_s(:js_date)
