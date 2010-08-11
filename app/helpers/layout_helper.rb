085c8131b9cc7924dfce4e7a41d861be030c92ce
ilake <lake.ilakela+github@gmail.com>
2010-08-11 16:57:18 +0800

    加入fb like

--- app/helpers/layout_helper.rb
+++ app/helpers/layout_helper.rb
@@ -34,4 +34,8 @@ module LayoutHelper
      link_to(current_user_location, user_location_url, :rel => "facebox")
   end
 
+  def facebook_like
+    content_tag :iframe, nil, :src => "http://www.facebook.com/plugins/like.php?href=#{CGI::escape(request.url)}&layout=standard&show_faces=true&width=450&action=like&font=arial&colorscheme=light&height=80", :scrolling => 'no', :frameborder => '0', :allowtransparency => true, :id => :facebook_like
+  end
+
 end
