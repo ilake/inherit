60960152068bc865ae6c58191ec90f17ef423491
ilake <lake.ilakela+github@gmail.com>
2010-09-08 17:48:02 +0800

    處理網站沒資料 沒有tag的問題(2)

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -1,10 +1,10 @@
 module SearchHelper
   def search_page_meta_tag(questions, goals, experiences)
-    if !experiences.blank?
+    if !experiences.blank?  && experiences.last
       "#{truncate_u(Sanitize.clean(experiences.last.content), 100)}"
-    elsif !questions.blank?
+    elsif !questions.blank? && questions.last
       "#{truncate_u(Sanitize.clean(questions.last.content), 100)}"
-    elsif !goals.blank?
+    elsif !goals.blank? && goals.last
       "#{truncate_u(Sanitize.clean(goals.last.content), 100)}"
     else
       ""
